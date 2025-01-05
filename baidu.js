c8350be4acf0cd565b1346535ea56b26b01f89ca
chendx97 <3310115926@qq.com>
2025-01-05 20:46:41 +0800

    Site updated: 2025-01-05 20:46:38

---
+++ baidu.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5173976cbe17152fbeefbfe025ecfd84";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
